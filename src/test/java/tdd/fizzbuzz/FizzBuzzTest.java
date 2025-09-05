86791130690bc91ee746e4eb074d1a7e464e1306
LEECH3 <leech3@oocl.com>
2025-09-05 11:04:54 +0800

    Feature: add countOff 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,24 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_1_when_order_1() {
+        // given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int order = 1;
+
+        // when
+        String word = fizzBuzz.countOff(order);
+
+        // then
+        String exceptedResult = "1";
+
+        assertEquals(exceptedResult, word);
+    }
+
 }
